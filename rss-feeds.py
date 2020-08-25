5ddab677aa6345f0630086a5ea9a55f29a77ba04
Jimit Dholakia <jimitd101@gmail.com>
2020-08-25 14:57:39 +0530

    Create rss-feeds.py

---
+++ rss-feeds.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Jimit.Dholakia
+"""
+
+from datetime import datetime, timedelta
+import time
+import os
+import itertools
+import feedparser
+import urllib.parse
+
+os.environ['TZ'] = 'Asia/Kolkata'
+time.tzset()
+
+RSS_FEEDS = ['https://www.pyimagesearch.com/feed/',
+             'https://medium.com/feed/@jimit105',
+             'https://machinelearningmastery.com/feed/',
+             'https://www.fast.ai/atom.xml',
+             'https://openai.com/blog/rss/',
+             'https://thecodelens.blogspot.com/feeds/posts/default',
+             'https://research.fb.com/feed/',
+             'http://googleaiblog.blogspot.com/atom.xml',
+             'https://blogs.microsoft.com/ai/feed/',
+             'https://www.analyticsvidhya.com/feed/'
+             ]
+
+
+def convert_timezone(datetime_struct_time):
+    dt = datetime.fromtimestamp(time.mktime(datetime_struct_time))
+    dt2 = dt - timedelta(seconds=time.timezone)
+    return dt2
+
+
+def fetch_feeds(feed_url):
+    feed = feedparser.parse(feed_url)
+    output = []
+
+    for entry in feed.entries:
+        #        article = '<li><a href="' +  + '">' +  + '</a></li>'
+        article = '<p><a href="' + entry.link + '">' + entry.title + '</a><br/>' + feed.feed.title + \
+            ' | ' + \
+            convert_timezone(entry.updated_parsed).strftime(
+                '%b %d, %Y %X %Z') + '</p>'
+        output.append((article, convert_timezone(entry.updated_parsed)))
+
+    return output
+
+
+result = list(map(fetch_feeds, RSS_FEEDS))
+merged = list(itertools.chain(*result))
+output = sorted(merged, key=lambda x: x[-1], reverse=True)
+
+
+all_articles = ''
+for article in output:
+    all_articles += article[0]
+
+current_time = time.strftime('%b %d, %Y %X %Z', time.localtime())
+action_badge = '![RSS Feeds Update](https://github.com/jimit105/rss-feeds-articles/workflows/RSS%20Feeds%20Update/badge.svg?branch=master)'
+header = action_badge + '\n![Last Updated](https://img.shields.io/badge/Last%20Updated%20on-' + \
+    urllib.parse.quote(current_time) + '-brightgreen)' + '\n\n'
+
+
+complete_text = header + all_articles
+
+with open('README.md', 'w') as f:
+    f.write(complete_text)
+
+print('RSS Feeds Update Complete')
