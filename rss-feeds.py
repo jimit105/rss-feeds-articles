d23fd487c733a90ac9bfe9db21f55dcb8bf47d1f
Jimit Dholakia <jimit105@users.noreply.github.com>
2020-09-01 15:06:51 +0530

    Update rss-feeds.py

--- rss-feeds.py
+++ rss-feeds.py
@@ -32,6 +32,7 @@ RSS_FEEDS = ['https://medium.com/feed/@jimit105',
              'https://mlfromscratch.com/rss/',
              'https://nanonets.com/blog/rss/',
              'https://www.mygreatlearning.com/blog/category/artificial-intelligence/feed/',
+             'https://jimit105.github.io/medium-articles/rss.xml',
              ]
 
 TOP_N = 200
