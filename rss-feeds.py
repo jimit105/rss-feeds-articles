6229cdf8a9dccf8471ae1710c79168a4ffa44718
Jimit Dholakia <jimit105@users.noreply.github.com>
2020-09-08 10:43:33 +0530

    Add KDnuggets

--- rss-feeds.py
+++ rss-feeds.py
@@ -33,7 +33,8 @@ RSS_FEEDS = ['https://medium.com/feed/@jimit105',
              'https://mlfromscratch.com/rss/',
              'https://nanonets.com/blog/rss/',
              'https://www.mygreatlearning.com/blog/category/artificial-intelligence/feed/',
-             'https://blog.tensorflow.org/atom.xml'
+             'https://blog.tensorflow.org/atom.xml',
+             'http://feeds.feedburner.com/kdnuggets-data-mining-analytics'
              ]
 
 TOP_N = 200
