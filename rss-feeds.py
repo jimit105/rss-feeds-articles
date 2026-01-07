18f448a12ab6b7e4412729f72d070ee32cab06b2
Jimit Dholakia <jimitd101@gmail.com>
2026-01-06 23:15:06 -0800

    Add Substack RSS feed to the list of feeds

--- rss-feeds.py
+++ rss-feeds.py
@@ -20,6 +20,7 @@ if os.name != 'nt':
 TIME_FMT = '%b %d, %Y %H:%M:%S %Z'
 
 RSS_FEEDS = ['https://jimit105.medium.com/feed',
+             'https://jimit105.substack.com/feed',
              'https://jimit105.github.io/pytricks/rss.xml',
              'https://scitech105.blogspot.com/rss.xml',
              'https://jimit105.github.io/medium-articles/rss.xml',
